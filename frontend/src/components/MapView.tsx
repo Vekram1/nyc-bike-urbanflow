f334b590d02fd27b22edeaed6d9130c9a986dddb
Vekram1 <oddirajuvikram@gmail.com>
2026-01-26 23:03:05 -0500

    Add MapLibre placeholder shell

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -1,5 +1,18 @@
 "use client";
 
 export default function MapView() {
-  return <div>MapView</div>;
+  return (
+    <div
+      style={{
+        height: "480px",
+        borderRadius: "12px",
+        background: "#e9e3d8",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      MapLibre + deck.gl placeholder
+    </div>
+  );
 }
