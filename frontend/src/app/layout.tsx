8ca8dfe5b5e56efb34d9c686ad10a18dd9911212
Vekram1 <oddirajuvikram@gmail.com>
2026-01-27 19:13:11 -0500

    Load Mapbox CSS

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import "../styles/theme.css";
 import "../styles/tokens.css";
-import "maplibre-gl/dist/maplibre-gl.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 import Providers from "./providers";
 
